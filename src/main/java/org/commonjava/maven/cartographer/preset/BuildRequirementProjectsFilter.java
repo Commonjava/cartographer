389938723b467bcd3800cfff04c2fe694899290b
Petr Kocandrle <pkocandr@redhat.com>
2015-01-16 19:36:03 +0100

    Avoid potential null pointer access

--- src/main/java/org/commonjava/maven/cartographer/preset/BuildRequirementProjectsFilter.java
+++ src/main/java/org/commonjava/maven/cartographer/preset/BuildRequirementProjectsFilter.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
@@ -74,7 +74,7 @@ public class BuildRequirementProjectsFilter
         //        logger.info( "Creating filter {}",
         //                     runtimeOnly ? "for runtime artifacts ONLY - only dependencies in the runtime/compile scope."
         //                                     : "for any artifact" );
-        this( runtimeOnly, acceptManaged, acceptBOMs, excludes, 
+        this( runtimeOnly, acceptManaged, acceptBOMs, excludes,
               runtimeOnly ? new OrFilter( new DependencyFilter( DependencyScope.runtime, ScopeTransitivity.maven, false,
                                                                 true, excludes ),
                                           new DependencyFilter( DependencyScope.embedded, ScopeTransitivity.maven, false,
@@ -159,10 +159,13 @@ public class BuildRequirementProjectsFilter
                     if ( excludes != null )
                     {
                         exc = new HashSet<ProjectRef>( excludes );
+                        exc.addAll( dr.getExcludes() );
+                        construct = exc.equals( dr.getExcludes() );
                     }
-
-                    exc.addAll( dr.getExcludes() );
-                    construct = exc.equals( dr.getExcludes() );
+                    else
+                    {
+                        construct = true;
+                    }
                 }
 
                 boolean nextRuntimeOnly = runtimeOnly;
