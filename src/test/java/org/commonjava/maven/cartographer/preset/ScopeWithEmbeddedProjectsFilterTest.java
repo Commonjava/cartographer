d8eaeeb2a58d6035155417c95ed280ce24782f57
Petr Kocandrle <pkocandr@redhat.com>
2015-01-16 17:22:00 +0100

    Fix preset tests to not expect indirect BOM relationships to be accepted

--- src/test/java/org/commonjava/maven/cartographer/preset/ScopeWithEmbeddedProjectsFilterTest.java
+++ src/test/java/org/commonjava/maven/cartographer/preset/ScopeWithEmbeddedProjectsFilterTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
@@ -111,7 +111,7 @@ public class ScopeWithEmbeddedProjectsFilterTest
 
         final ProjectRelationshipFilter child = filter.getChildFilter( dep );
 
-        assertConcreteAcceptance( child, from, src, tgt, new HashSet<DependencyScope>(), BOM, PARENT );
+        assertConcreteAcceptance( child, from, src, tgt, new HashSet<DependencyScope>(), PARENT );
 
         assertRejectsAllManaged( child, from, src, tgt );
     }
@@ -125,13 +125,13 @@ public class ScopeWithEmbeddedProjectsFilterTest
 
         final ProjectRelationshipFilter child = filter.getChildFilter( dep );
 
-        assertConcreteAcceptance( child, from, src, tgt, new HashSet<DependencyScope>(), BOM, PARENT );
+        assertConcreteAcceptance( child, from, src, tgt, new HashSet<DependencyScope>(), PARENT );
 
         assertRejectsAllManaged( child, from, src, tgt );
     }
 
     @Test
-    public void acceptAllEmbeddedAndRuntimeAndCompileDependenciesWithParentsAndBomsAfterTraversingRuntimeDependency()
+    public void acceptAllEmbeddedAndRuntimeAndCompileDependenciesWithParentsAfterTraversingRuntimeDependency()
         throws Exception
     {
         final DependencyRelationship dep =
@@ -141,13 +141,13 @@ public class ScopeWithEmbeddedProjectsFilterTest
 
         assertConcreteAcceptance( child, from, src, tgt,
                                   new HashSet<DependencyScope>( Arrays.asList( embedded, runtime, compile ) ),
-                                  DEPENDENCY, PARENT, BOM );
+                                  DEPENDENCY, PARENT );
 
         assertRejectsAllManaged( child, from, src, tgt );
     }
 
     @Test
-    public void acceptAllEmbeddedAndRuntimeAndCompileDependenciesWithParentsAndBomsAfterTraversingCompileDependency()
+    public void acceptAllEmbeddedAndRuntimeAndCompileDependenciesWithParentsAfterTraversingCompileDependency()
         throws Exception
     {
         final DependencyRelationship dep =
@@ -157,7 +157,7 @@ public class ScopeWithEmbeddedProjectsFilterTest
 
         assertConcreteAcceptance( child, from, src, tgt,
                                   new HashSet<DependencyScope>( Arrays.asList( embedded, runtime, compile ) ),
-                                  DEPENDENCY, PARENT, BOM );
+                                  DEPENDENCY, PARENT );
 
         assertRejectsAllManaged( child, from, src, tgt );
     }
