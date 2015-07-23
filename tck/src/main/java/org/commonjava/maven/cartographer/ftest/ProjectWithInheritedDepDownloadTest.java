d7efa7abb8daad2a2a73170fb0b4eba55c249690
Petr Kocandrle <pkocandr@redhat.com>
2015-07-23 18:26:02 +0200

    Add more tests

---
+++ tck/src/main/java/org/commonjava/maven/cartographer/ftest/ProjectWithInheritedDepDownloadTest.java
@@ -0,0 +1,39 @@
+package org.commonjava.maven.cartographer.ftest;
+
+import org.apache.maven.model.Model;
+import org.commonjava.maven.cartographer.dto.PomRecipe;
+import org.junit.Test;
+
+public class ProjectWithInheritedDepDownloadTest
+    extends AbstractCartographerTCK
+{
+
+    private static final String PROJECT = "dep-from-parent";
+
+    @Test
+    public void run()
+        throws Exception
+    {
+        final String dto = "pom.json";
+        final String depsTxt = "deps.txt";
+        final String repoResource = "/repo/org/foo/consumer/1/consumer-1.pom";
+        final int repoResourceTrim = 5;
+        final String alias = "test";
+
+        aliasRepo( alias, repoResource, repoResourceTrim );
+
+        final PomRecipe recipe = readRecipe( dto, PomRecipe.class );
+
+        final Model pom = carto.getRenderer()
+                               .generatePOM( recipe );
+
+        assertPomDeps( pom, false, depsTxt );
+    }
+
+    @Override
+    protected String getTestDir()
+    {
+        return PROJECT;
+    }
+
+}
