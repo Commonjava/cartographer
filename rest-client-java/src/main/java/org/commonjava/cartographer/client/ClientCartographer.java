87152efe030ae52a1860bb45af2d74170f233f42
John Casey <jdcasey@commonjava.org>
2016-09-07 17:43:36 -0500

    adjusting license headers

--- rest-client-java/src/main/java/org/commonjava/cartographer/client/ClientCartographer.java
+++ rest-client-java/src/main/java/org/commonjava/cartographer/client/ClientCartographer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011 Red Hat, Inc. (jdcasey@commonjava.org)
+ * Copyright (C) 2013 Red Hat, Inc. (jdcasey@commonjava.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,8 +31,6 @@ import org.commonjava.util.jhttpc.HttpFactory;
 import org.commonjava.util.jhttpc.auth.ClientAuthenticator;
 import org.commonjava.util.jhttpc.auth.PasswordManager;
 import org.commonjava.util.jhttpc.model.SiteConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
